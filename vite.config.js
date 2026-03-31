e75f888acbf9a3b2408c00a526f0a9e942d1b9c5
Bradmyersdigital <bradmyersdigital@gmail.com>
2026-03-31 17:11:30 -0500

    fix vite config to handle js and jsx

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+
 export default defineConfig({
-  plugins: [react()],
-  esbuild: { loader: 'jsx', include: /src\/.*\.js$/ }
+  plugins: [react({
+    include: '**/*.{jsx,js}',
+  })],
+  resolve: {
+    extensions: ['.jsx', '.js', '.tsx', '.ts']
+  }
 })
